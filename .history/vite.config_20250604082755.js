347781d25f79531278cf68dfbdc85f95f15badad
syed bayzid <husains82849@gmail.com>
2025-06-04 08:36:06 +0600

    project setuped

---
+++ .history/vite.config_20250604082755.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), 
+        tailwindcss(),
+    tailwindcss(),
+
+  ],
+})
+
+
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+export default defineConfig({
+  plugins: [
+  ],
+})
