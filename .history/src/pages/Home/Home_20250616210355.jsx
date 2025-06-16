a9f727479d439696f6ad85fabcbd12e8962a2a02
syed bayzid <husains82849@gmail.com>
2025-06-16 21:46:28 +0600

    react helmet problems fixed

---
+++ .history/src/pages/Home/Home_20250616210355.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Hero from '../../components/hero/Hero';
+import { useLoaderData } from 'react-router';
+import HomeCourseComponent from '../../components/HomeCoureseComponent/HomeCoureseComponent';
+import Testimonials from '../../components/Testimonials/Testimonials';
+import StayUpdateSection from '../../components/stayUpdateSection/stayUpdateSection';
+import { Helmet } from 'react-helmet';
+
+const Home = () => {
+    const courses = useLoaderData();
+    console.log(courses.lengt);
+    return (
+        <div className="max-w-6xl mx-auto">
+            <Helmet>
+                <title>Home Page</title>
+            </Helmet>
+            <Hero></Hero>
+            <HomeCourseComponent courses={courses}></HomeCourseComponent>
+            <Testimonials></Testimonials>
+            <StayUpdateSection></StayUpdateSection>
+        </div>
+    );
+};
+
+export default Home;
